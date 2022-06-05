50aaff72c1daae21f9c86775e31bfa17a9599aa8
Rendi Jaka Susanto <rendi.susanto@spesolution.com>
2022-06-05 21:40:56 +0700

    done

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sort) => {
+  let name = sort(names);
+  let array = [];
+  let number = 1;
+  for (i = 0; i < name.length; i++){
+    array.push(number++ + '. ' + name[i]);
+  }
+  return array;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  const sort = names.sort();
+  return sort;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  const sort = names.sort();
+  const descending = sort.reverse();
+  return descending;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
